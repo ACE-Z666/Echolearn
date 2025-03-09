9731df249c66b2738125fa567d9d85a60c89babf
Ashwin9633 <ashwinmanikuttan2004@gmail.com>
2025-03-09 17:33:34 +0530

    my 1st change

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -95,7 +95,7 @@ const Login = () => {
             Welcome To  <br /><span>EchoLearn!!</span>
         </h1>
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariat.
+        Lorem ipsum Ashwin dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariat.
         </p>
       </div>
     </div>
