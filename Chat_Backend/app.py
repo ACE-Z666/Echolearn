bcebcb6671f516096096a1a8ac00787899e6b75a
Abhijith_J_Nair <136841851+ACE-Z666@users.noreply.github.com>
2025-06-19 21:00:31 +0530

    CORS Update

--- Chat_Backend/app.py
+++ Chat_Backend/app.py
@@ -46,7 +46,7 @@ app = FastAPI(
 # Configure CORS for Railway deployment
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://echolearn-i7u7.onrender.com", "http://localhost:5173"],  # Allow all origins
+    allow_origins=["http://localhost:5173"],  # Allow all origins
     allow_methods=["*"],
     allow_headers=["*"],
 )
