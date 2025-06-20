f41c3e47d4be3248598c41e2d7c2351d8ca39190
Abhijith_J_Nair <136841851+ACE-Z666@users.noreply.github.com>
2025-06-20 18:51:06 +0530

    CORS issue

--- Chat_Backend/app.py
+++ Chat_Backend/app.py
@@ -135,6 +135,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",  # Changed from 0.0.0.0 to 127.0.0.1 for local development
-        port=8000,
+        port=10000,
         reload=True,  # Enable auto-reload for development
     )
